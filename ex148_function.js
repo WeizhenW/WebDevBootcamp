9044f99b1ca3585f800915fa5cd1632ab7a98bd2
Weizhen Wang <wangwzhen@gmail.com>
2019-04-03 17:30:02 -0500

    function exer

---
+++ ex148_function.js
@@ -0,0 +1,23 @@
+function isEven(num) {
+  if(num % 2 === 0) {
+    return true;
+  }
+  return false;
+}
+
+function factorial(num) {
+  if(num === 0) {
+    return 1;
+  }
+  else {
+    var result = 1;
+    for(var i=1; i <= num; i++) {
+    result *= i;
+    }
+    return result;
+  }
+}
+
+function kebabToSnake(str) {
+  return str.replace("-", "_");
+}

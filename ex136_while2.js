f120f0ef44ad2f2088d61ab15d8fea573ced5867
Weizhen Wang <wangwzhen@gmail.com>
2019-04-03 14:29:44 -0500

    another while loop

---
+++ ex136_while2.js
@@ -0,0 +1,14 @@
+var answer = prompt("Are we there yet?");
+
+// while(answer !== "yes" && answer !== "yeah") {
+//   var answer = prompt("Are we there yet?");
+// }
+//
+// alert("Yay, we finally made it!");
+
+
+while(answer.indexOf("yes") === -1 && answer.indexOf("yeah") === -1) {
+  var answer = prompt("Are we there yet?");
+}
+
+alert("Yay, we finally made it!");

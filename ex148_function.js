4fe30b53f238f075ffd06920d00fe04962700c67
Weizhen Wang <wangwzhen@gmail.com>
2019-04-03 17:45:00 -0500

    function exer refined answer

--- ex148_function.js
+++ ex148_function.js
@@ -1,23 +1,24 @@
 function isEven(num) {
-  if(num % 2 === 0) {
-    return true;
-  }
-  return false;
+  // if(num % 2 === 0) {
+  //   return true;
+  // }
+  // return false;
+  return num % 2 === 0;
 }
 
 function factorial(num) {
-  if(num === 0) {
-    return 1;
-  }
-  else {
-    var result = 1;
-    for(var i=1; i <= num; i++) {
+  // if(num === 0) {
+  //   return 1;
+  // }
+  // else {
+  var result = 1;
+  for(var i=1; i <= num; i++) {
     result *= i;
     }
     return result;
   }
-}
+
 
 function kebabToSnake(str) {
-  return str.replace("-", "_");
+  return str.replace(/-/g, "_");
 }

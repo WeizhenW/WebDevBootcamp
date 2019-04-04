23761e5b22b2dd3fd3ca4150c8937900d35497bd
Weizhen Wang <wangwzhen@gmail.com>
2019-04-04 09:23:04 -0500

    todo part 2 added

--- ex158_todo1.js
+++ ex158_todo1.js
@@ -5,12 +5,25 @@ window.setTimeout(function() {
 
   while(input !== "quit") {
     if(input === "list") {
-    console.log(toDo);
+      console.log("********************");
+      toDo.forEach(function(item){
+        console.log(toDo.indexOf(item) + ": " + item);
+      })
+      console.log("********************");
     }
     else if(input === "new") {
-     var newElement = prompt("Please enter the new item:");
-     toDo.push(newElement);
+      var newElement = prompt("Please enter the new item:");
+      toDo.push(newElement);
+      console.log(newElement + " has been added to the list");
+    }
+    else if(input === "delete") {
+      var delIndex = prompt ("Please enter the index to delete:");
+      toDo.splice(delIndex, 1);
+      console.log("Todo is removed");
+
     }
   input = prompt("What would you like to do?");
  }
-})
+
+ console.log("OK you quit the list");
+}, 500);

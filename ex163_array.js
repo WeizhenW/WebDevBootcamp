64b99e0831316af8c660c469b14b5e4bf1d29bcb
Weizhen Wang <wangwzhen@gmail.com>
2019-04-04 13:36:23 -0500

    function with array

---
+++ ex163_array.js
@@ -0,0 +1,22 @@
+function printReverse(inputArray){
+  for(i = inputArray.length - 1; i >= 0; i--){
+    console.log(inputArray[i]);
+  }
+}
+
+function isUniform(inputArray) {
+  for(i = 0; i < inputArray.length-1; i++){
+    if(inputArray[i] !== inputArray[i+1]){
+      return false;
+    }
+  }
+  return true;
+}
+
+function sumArray(inputArray){
+  var total = 0;
+  inputArray.forEach(function(item){
+    total = total + item;
+  })
+  return total;
+}

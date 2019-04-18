883ef7ef509200cab381af97c0ae6c46dd070b36
Weizhen Wang <wangwzhen@gmail.com>
2019-04-17 22:08:45 -0500

    refactor by using classList.toggle

--- ex187_dom_colortoggle.js
+++ ex187_dom_colortoggle.js
@@ -1,14 +1,18 @@
 let button = document.querySelector("button");
 let body = document.querySelector("body");
-let isPurple = false;
+// let isPurple = false;
 
-button.addEventListener("click", function(){
-  if(isPurple) {
-    body.style.background = "white";
-    // isPurple = false;
-  } else {
-    body.style.background = "purple";
-    // isPurple = true;
-  }
-  isPurple = !isPurple;
-});
+// button.addEventListener("click", function(){
+//   if(isPurple) {
+//     body.style.background = "white";
+//     // isPurple = false;
+//   } else {
+//     body.style.background = "purple";
+//     // isPurple = true;
+//   }
+//   isPurple = !isPurple;
+// });
+
+button.addEventListener("click", function() {
+  body.classList.toggle("purple");
+})

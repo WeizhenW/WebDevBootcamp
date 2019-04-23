15a7fd5326038f11d1ba520f42f1180d798c7deb
Weizhen Wang <wangwzhen@gmail.com>
2019-04-23 10:46:33 -0500

    partially refactored

--- ex195_color.js
+++ ex195_color.js
@@ -7,40 +7,66 @@ let pickedColor = pickColor();
 let colorDisplay = document.getElementById("targetColor");
 let messageDisplay = document.getElementById("message");
 let restart = document.getElementById("restart");
-let easyButton = document.getElementById("easyButton");
-let hardButton = document.getElementById("hardButton");
-
+// let easyButton = document.getElementById("easyButton");
+// let hardButton = document.getElementById("hardButton");
+let modeButtons = document.querySelectorAll(".mode");
 //the first display of the colors
 refreshColor();
 compareColor();
 
-//if easy clicked, only 3 colors
-easyButton.addEventListener("click", function() {
-  this.classList.add("selected");
-  hardButton.classList.remove("selected");
-  for(let i=0; i<secondRow.length;i++) {
-    secondRow[i].style.display = "none";
-  } //the returned value of secondRow is an array;
-  //go through each element of this array to apply the background color (hide the second row effect)
-  numOfColors = 3;
-  colors = generateRandomColors(numOfColors);
-  pickedColor = pickColor();
-  refreshColor();
-  compareColor();
+for(let i=0; i<modeButtons.length; i++) {
+  modeButtons[i].addEventListener("click", function(){
+    modeButtons[0].classList.remove("selected");
+    modeButtons[1].classList.remove("selected");
+    this.classList.add("selected");
+    if(this.textContent === "EASY") {
+      numOfColors = 3;
+      for(let j=3; j<6;j++) {
+        squares[j].style.display = "none";
+        }
+      //the returned value of secondRow is an array;
+      //go through each element of this array to apply the hide
+    } else {
+      numOfColors = 6;
+      for(let j=3; j<6; j++) {
+        squares[j].style.display = "block";
+      }
+    }
+    colors = generateRandomColors(numOfColors);
+    pickedColor = pickColor();
+    refreshColor();
+    compareColor();
+
 })
-//if hard clicked, all 6 colors
-hardButton.addEventListener("click", function() {
-  this.classList.add("selected");
-  easyButton.classList.remove("selected");
-  for(let i=0; i<secondRow.length;i++) {
-    secondRow[i].style.display = "block";
-  }
-  numOfColors = 6;
-  colors = generateRandomColors(numOfColors);
-  pickedColor = pickColor();
-  refreshColor();
-  compareColor();
-})
+}
+// //if easy clicked, only 3 colors
+// easyButton.addEventListener("click", function() {
+//   this.classList.add("selected");
+//   hardButton.classList.remove("selected");
+//   for(let i=0; i<secondRow.length;i++) {
+//     secondRow[i].style.display = "none";
+//   } //the returned value of secondRow is an array;
+//   //go through each element of this array to apply the background color (hide the second row effect)
+//   numOfColors = 3;
+//   colors = generateRandomColors(numOfColors);
+//   pickedColor = pickColor();
+//   refreshColor();
+//   compareColor();
+// })
+// //if hard clicked, all 6 colors
+// hardButton.addEventListener("click", function() {
+//   this.classList.add("selected");
+//   easyButton.classList.remove("selected");
+//   for(let i=0; i<secondRow.length;i++) {
+//     secondRow[i].style.display = "block";
+//   }
+//   numOfColors = 6;
+//   colors = generateRandomColors(numOfColors);
+//   pickedColor = pickColor();
+//   refreshColor();
+//   compareColor();
+// })
+
 //refresh to get new colors
 restart.addEventListener("click", function() {
   header.style.backgroundColor = "steelblue";
@@ -62,6 +88,21 @@ function refreshColor() {
   }
   // console.log(colors);
 }
+
+//function to generate all the candidate colors
+function generateRandomColors(num) {
+  let colorArray = [];
+  for(let k=0; k<num; k++) {
+    let r = Math.floor(Math.random() * 255);
+    let j = Math.floor(Math.random() * 255);
+    let b = Math.floor(Math.random() * 255);
+    let newColor = "rgb(" + r + ", " + j + ", " + b + ")";
+    // console.log(newColor);
+    colorArray.push(newColor);
+    }
+  console.log("colorArray:", colorArray);
+  return colorArray;
+  }
 
 //function to compare if the clicked color matches the picked color
 function compareColor() {
@@ -88,24 +129,9 @@ function colorChange() {
     squares[i].style.backgroundColor = pickedColor;
   }
   header.style.backgroundColor = pickedColor;
-
 }
 //function to set a random picked color
 function pickColor() {
   let indexColor = Math.floor(Math.random() * colors.length); //easy vs. hard mode
   return colors[indexColor];
 }
-//function to generate all the candidate colors
-function generateRandomColors(num) {
-  let colorArray = [];
-  for(let i=0; i<num; i++) {
-    let r = Math.floor(Math.random() * 255);
-    let j = Math.floor(Math.random() * 255);
-    let b = Math.floor(Math.random() * 255);
-    let newColor = "rgb(" + r + ", " + j + ", " + b + ")";
-    // console.log(newColor);
-    colorArray.push(newColor);
-    }
-  console.log("colorArray:", colorArray);
-  return colorArray;
-  }

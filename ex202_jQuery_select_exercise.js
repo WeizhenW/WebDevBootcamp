b239de5970b9d51a9e153be6dc47220725016d61
Weizhen Wang <wangwzhen@gmail.com>
2019-04-24 09:25:15 -0500

    jQuery selecting exercise completed

--- ex202_jQuery_select_exercise.js
+++ ex202_jQuery_select_exercise.js
@@ -1,4 +1,7 @@
 $("div").css("background", "purple");
 $(".highlight").css("width", "200px");
 $("#third").css("border", "orange solid 2px");
-$("h1+div").css("color","pink");
+//how to select only the 1st div
+$("div:first-of-type").css("color","pink");
+//or
+//$("div:first").css("color", "pink");

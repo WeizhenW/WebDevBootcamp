f33550a60cafbaaef5aaef2f002bac57d437d309
Weizhen Wang <wangwzhen@gmail.com>
2019-04-24 12:03:41 -0500

    events added

---
+++ ex213_click.js
@@ -0,0 +1,20 @@
+// $("h1").click(function() {
+//   alert("Ah, h1 is clicked!");
+// })
+//
+// $("button").click(function() {
+//   $(this).css("background", "yellow");
+//   let text = $(this).text();
+//   console.log(text, "is clicked!");
+// })
+//
+// $("input").keypress(function(event) {
+//     if(event.which === 13) { //when the enter key is hit
+//         console.log($(this).val());
+//         $(this).val("");//clear the input content
+//     }
+// })
+
+$("h1").on("dblclick", function() {
+    $(this).css("color", "red");
+})

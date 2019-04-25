da34b0565d148aa8079bdcc6950b917d9b883e9b
Weizhen Wang <wangwzhen@gmail.com>
2019-04-24 23:11:15 -0500

    finalized

--- ex220_todo.js
+++ ex220_todo.js
@@ -1,23 +1,19 @@
-let newTodo = $("input").val();
 
-//click to cross over
-$("li").on("click", function() {
-  $(this).toggleClass("crossOver");
-})
-
-//append newTodo to the end
+//append new to do item to the end
 $("input").on("keypress", function(event) {
   if(event.which === 13) {
-    $("ul").append("<li><i class='fas fa-trash-alt'></i>" + $(this).val() + "</li>");
-    $("input").val("");
+    let newTodo = $(this).val();
+    $("ul").append("<li><span class='trash'><i class='fas fa-trash-alt'></i></span><span id='item'>" + newTodo +"</span></li>");
+    $(this).val("");
   }
 })
 
-//hove over to show trash Cleaning
+//click to cross over to do items
+$("ul").on("click", "li", function() {
+  $(this).toggleClass("crossOver");
 
-$("li").on("click", function() {
-  $(".fa-trash-alt").fadeIn(1000);
-})
+})//on will help add the listener to non existing element => li not existing yet at the beginning
+//need to apply to ul first => but only fire when li is clicked
 
 //plus sign to hide and show the form
 $("#plusSign").on("click", function() {
@@ -25,6 +21,9 @@ $("#plusSign").on("click", function() {
 })
 
 //click trash can to delete item
-$("fa-trash-alt").on("click", function() {
-  $(this).parent().css("visibility", "hidden")
+$("ul").on("click", ".trash", function() {
+  event.stopPropagation();
+  $(this).parent().fadeOut(500, function() {
+    $(this).remove();
+  });  // $(this).parent().remove();
 })
